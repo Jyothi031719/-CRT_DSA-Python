4ecf5160955b938e3aa4c64b794c64bcc65031d0
Jyothi031719 <anugondajyothi06@gmail.com>
2026-06-19 05:05:55 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+class  student:
+    def __init__(self,name,grade):
+      self.name = name 
+      self.grade = grade 
+s1 = student("kalyan","jyothi")
+print(s1.grade)
+s1.grade = "B"
+print(s1.grade)
+
+
